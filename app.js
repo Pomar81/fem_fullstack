69121f18c0a9ef85f949971580e26ab17cfe5231
spomaraiko <spomaraiko@exadel.com>
2023-10-03 12:37:06 +0300

    rename

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const http = require('node:http')
 const fs = require('node:fs')
 const PORT = 3000
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
     res.writeHead(200, {'content-type': 'text/html'})
     fs.createReadStream('index.html').pipe(res)
 } )
 
-server.listen(PORT)
+app.listen(PORT)
 console.log(`Server starter on port ${PORT}`)
 
 
