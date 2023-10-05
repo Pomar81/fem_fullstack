4a0ff1a7d6acd902a9efb094aca7c2edfd98d522
spomaraiko <spomaraiko@exadel.com>
2023-10-05 13:55:33 +0300

    use express server

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const http = require('node:http')
-const fs = require('node:fs')
-const PORT = 3000
+const express = require('express')
+const server = require('node:http').createServer()
+const app = express()
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/html'})
-    fs.createReadStream('index.html').pipe(res)
-} )
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+})
 
-app.listen(PORT)
-console.log(`Server starter on port ${PORT}`)
+server.on('request', app)
 
+server.listen(3000, () => {console.log('Listening on 3000')})
 
