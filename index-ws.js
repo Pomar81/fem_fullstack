2d76a1aaec0b76738d269425ce5f50b260646eaa
spomaraiko <spomaraiko@exadel.com>
2023-10-06 18:25:36 +0300

    add database

--- index-ws.js
+++ index-ws.js
@@ -1,36 +1,85 @@
-const express = require('express')
-const server = require('node:http').createServer()
-const app = express()
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname})
-})
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname});
+});
 
-server.on('request', app)
-server.listen(3000, () => {console.log('Listening on 3000')})
+server.on('request', app);
+server.listen(3000, function() { console.log('server started on port 3000'); });
+
+server.on('close', () => {console.log('server is closed')})
+
 
 /** Begin websocket */
-const WebSocketServer = require('ws').Server
+const WebSocketServer = require('ws').Server;
 
-const wss = new WebSocketServer({server})
-
-wss.on('connection', (ws) => {
-    const numClients = wss.clients.size
-    console.log('clients connected: ', numClients)
-
-    wss.broadcast(`Current visitors: ${numClients}`)
-
-    if(ws.readyState === ws.OPEN) {
-        ws.send('Welcome to my server')
-    }
-
-    ws.on('close', () => {
-        console.log('A client has disconnected')
+const wss = new WebSocketServer({server: server});
+wss.on('close', () => {console.log('wss is closed')})
+process.on('SIGINT', () => {
+    wss.close()
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
     })
+
 })
 
-wss.broadcast = (data) => {
-    wss.clients.forEach((client) => {
-        client.send(data)
-    })
+
+wss.on('connection', function connection(ws) {
+    const numClients = wss.clients.size;
+    console.log('Clients connected', numClients);
+
+    wss.broadcast(`Current visitors: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server');
+    }
+
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
+    ws.on('close', function close() {
+        wss.broadcast(`Current visitors: ${numClients}`);
+        console.log('A client has disconnected');
+    });
+
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+}
+
+/** end websockets */
+/** begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    console.log('Shutting down db');
+
+    getCounts();
+    db.close();
+
 }
